0e179eb4d1210f129f79abafe3f3ce2f4e9763ee
Caleb Seelhoff <cseelhoff@gmail.com>
2024-01-25 12:02:52 -0600

    auto-password

--- portainer/deploy-portainer.sh
+++ portainer/deploy-portainer.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 PORTAINER_URL="http://localhost:9000"
 USERNAME="admin"
-if [ -z ${PASSWORD+x} ]; then
-  echo "Enter Portainer password:"
-  read -s PASSWORD
-fi
-
-AUTH_PAYLOAD='{"Username": "'"$USERNAME"'",  "Password": "'"$PASSWORD"'"}'
+PORTAINER_PASSWORD="5wmjVNT8uRc+0dW1cguMd14SLYLnHGUV3JIjiIOkKXU="
+AUTH_PAYLOAD='{"Username": "'"$USERNAME"'",  "Password": "'"$PORTAINER_PASSWORD"'"}'
 RESPONSE=$(curl -s -X POST -H "Content-Type: application/json" -d "$AUTH_PAYLOAD" $PORTAINER_URL/api/auth)
 AUTH_TOKEN=$(echo $RESPONSE | jq -r .jwt)
 ENDPOINT_PAYLOAD='{"name": "local"}'
@@ -15,9 +11,3 @@ LOCAL_ENDPOINTID=$(curl -s -X GET -H "Authorization: Bearer $AUTH_TOKEN" -H "Con
 if [ "$LOCAL_ENDPOINTID" == "null" ]; then
   LOCAL_ENDPOINTID=$(curl -s -X POST $ENDPOINTS_URL -H "Authorization: Bearer $AUTH_TOKEN" -F "Name=local" -F "EndpointCreationType=1" | jq '.Id')
 fi
-
-STACK_NAME="openldap"
-DOCKER_COMPOSE_JSON_STRING=$(cat /opt/openldap/docker-compose.yml | jq -Rs .)
-STACK_PAYLOAD='{ "name": "'"$STACK_NAME"'", "stackFileContent": '"$DOCKER_COMPOSE_JSON_STRING"' }'
-STACKS_URL="$PORTAINER_URL/api/stacks?type=2&method=string&endpointId=$LOCAL_ENDPOINTID"
-curl -s -X POST -H "Authorization: Bearer $AUTH_TOKEN" -H "Content-Type: application/json" -d "$STACK_PAYLOAD" $STACKS_URL
--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-# login to vcenter using govc session.login and credentials from vcenter_settings.json
+if [ -z ${GOVC_PASSWORD+x} ]; then
+  echo "Enter vCenter password:"
+  read -s GOVC_PASSWORD
+fi
+
 export GOVC_INSECURE=true
 export CONNECTION=$(jq -r '.username + ":" + .password + "@" + .vcenter' vcenter_settings.json)
 export OVA_URL=$(jq -r '.ova_url' vcenter_settings.json)
 export GOVC_USERNAME=$(jq -r '.username' vcenter_settings.json)
-export GOVC_PASSWORD=$(jq -r '.password' vcenter_settings.json)
 export GOVC_URL=$(jq -r '.vcenter' vcenter_settings.json)
 export LIBRARY=$(jq -r '.library' vcenter_settings.json)
 export DATASTORE=$(jq -r '.datastore' vcenter_settings.json)
@@ -12,8 +15,10 @@ export VM_NAME=$(jq -r '.vm_name' vcenter_settings.json)
 export OVA_NAME=$(jq -r '.ova_name' vcenter_settings.json)
 export HOST=$(jq -r '.host' vcenter_settings.json)
 
-# create a new ssh key
-ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ''
+# if ~/.ssh/id_rsa does not exist, create it
+if [ ! -f ~/.ssh/id_rsa ]; then
+  ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ''
+fi
 # Read the public key from ~/.ssh/id_rsa.pub
 PUBLIC_KEY=$(cat ~/.ssh/id_rsa.pub)
 # Escape special characters in the public key to use in sed
@@ -21,11 +26,33 @@ ESCAPED_PUBLIC_KEY=$(echo "$PUBLIC_KEY" | sed -e 's/[\/&]/\\&/g')
 # Replace the existing key in coreos.bu with the new public key
 sed -i "s/ssh-rsa [^ ]*/$ESCAPED_PUBLIC_KEY/g" coreos.bu
 
+# gererate a random password
+PORTAINER_PASSWORD=$(openssl rand -base64 32)
+# Replace the existing password in portainer/deploy-portainer.sh with the new password
+sed -i "s/PORTAINER_PASSWORD=\"[^\"]*/PORTAINER_PASSWORD=\"$PORTAINER_PASSWORD/g" ./portainer/deploy-portainer.sh
+PORTAINER_BCRYPT=$(htpasswd -nbB admin $PORTAINER_PASSWORD | cut -d ":" -f 2)
+# replace all of the $ symbols in the bcrypt hash with two consecutive $$ symbols
+ESCAPED_PORTAINER_BCRYPT=$(echo "$PORTAINER_BCRYPT" | sed -e 's/\$/\$\$/g')
+sed -i "s/--admin-password '[^']*/--admin-password '$ESCAPED_PORTAINER_BCRYPT/g" ./coreos.bu
+
 butane --files-dir ./ --pretty --strict coreos.bu --output coreos.ign
 govc session.login -u $CONNECTION
 
-govc library.create -ds=$DATASTORE $LIBRARY
-govc library.import $LIBRARY $OVA_URL
+# use govc library.ls -json to check if the library exists
+if govc library.ls -json | jq -r '.[].name' | grep -q $LIBRARY; then
+  echo "Library $LIBRARY already exists"
+else
+  echo "Creating library $LIBRARY"
+  govc library.create -ds=$DATASTORE $LIBRARY
+fi
+
+# check if the OVA already exists in the library
+if govc library.ls -json $LIBRARY/* | jq -r '.[].name' | grep -q $OVA_NAME; then
+  echo "OVA $OVA_NAME already exists in library $LIBRARY"
+else
+  echo "Importing OVA $OVA_NAME into library $LIBRARY"
+  govc library.import $LIBRARY $OVA_URL
+fi
 
 govc library.deploy -host=$HOST /$LIBRARY/$OVA_NAME $VM_NAME
 #govc vm.create -m 4096 -c 2 -g coreos -net.adapter vmxnet3 -net="VM Network" -disk.controller pvscsi -disk.backing datastore1 -on=false coreos
@@ -35,10 +62,8 @@ govc vm.power -on $VM_NAME
 
 echo "Waiting for VM to be ready..."
 VM_IP=$(govc vm.ip $VM_NAME)
-#VM_IP="10.0.1.46"
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=5 -i ~/.ssh/id_rsa admin@$VM_IP
 
 # delete the VM
-#govc vm.power -off $VM_NAME
-#govc vm.destroy $VM_NAME
-
+govc vm.power -off $VM_NAME
+govc vm.destroy $VM_NAME
