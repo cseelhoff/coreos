360626dc79ad05673bd7205e5f76ccea09cdd1ec
Caleb Seelhoff <cseelhoff@gmail.com>
2024-01-28 20:01:14 +0000

    fix misspellings

--- bootstrap/traefik.sh
+++ bootstrap/traefik.sh
@@ -4,12 +4,13 @@ LOCAL_DOMAIN=us.177cpt.com
 CF_API_EMAIL=cseelhoff@gmail.com
 CF_API_KEY=fZ-U4Q3zgxHq6_Cn_a-_Ps0-SwL1FRg3Vd-cMp-S
 TIMEZONE=America/Chicago
-DNS_SERVER='10.0.1.1'
+DNS_SERVER='10.0.1.44'
 DHCP_ROUTER='10.0.1.2'
 DHCP_START='10.0.1.10'
 DHCP_END='10.0.1.50'
 
 PIHOLE_LOGIN_URL=http://localhost:8080/admin/login.php
+PIHOLE_INDEX_URL=http://localhost:8080/admin/index.php
 PIHOLE_CUSTOM_DNS_URL=http://localhost:8080/admin/scripts/pi-hole/php/customdns.php
 PIHOLE_PASSWORD=$(openssl rand -base64 32)
 TRAEFIK_PASSWORD=$(openssl rand -base64 32)
@@ -20,7 +21,7 @@ NEXUS_FQDN=nexus.$LOCAL_DOMAIN
 docker run -d \
     --name=pihole \
     -h pihole \
-    -p 53:53/tcp -p 53:53/udp -p 67:67/udp -p 80:8080/tcp \
+    -p 53:53/tcp -p 53:53/udp -p 67:67/udp -p 8080:80/tcp \
     -e DNSMASQ_LISTENING=all \
     -e TZ=$TIMEZONE \
     -e PIHOLE_DNS_=$DNS_SERVER \
@@ -29,7 +30,7 @@ docker run -d \
     -e DHCP_END=$DHCP_END \
     -e PIHOLE_DOMAIN=$LOCAL_DOMAIN \
     -e VIRTUAL_HOST=pihole \
-    -e WEBPASSWORD=$PASSWORD \
+    -e WEBPASSWORD=$PIHOLE_PASSWORD \
     -v $(pwd)/etc-pihole/:/etc/pihole/ \
     -v $(pwd)/etc-dnsmasq.d/:/etc/dnsmasq.d/ \
     --cap-add NET_ADMIN \
@@ -37,22 +38,24 @@ docker run -d \
     pihole/pihole:2024.01.0
 
 # Make the POST request and extract the token
-PIHOLE_TOKEN=$(curl -s -d "pw=$PIHOLE_PASSWORD" -X POST $PIHOLE_LOGIN_URL | grep -oP '(?<=<div id="token" hidden>)(\S+)(?=<\/div>)')
+PIHOLE_TOKEN=$(curl -s -d "pw=$PIHOLE_PASSWORD" -c cookies.txt -X POST $PIHOLE_INDEX_URL | grep -oP '(?<=<div id="token" hidden>)(\S+)(?=<\/div>)')
 # Add a A record for nexus
-curl -d "action=add&ip=$DNS_SERVER&domain=$PIHOLE_FQDN&token=$PIHOLE_TOKEN" -X POST $PIHOLE_CUSTOM_DNS_URL
-curl -d "action=add&ip=$DNS_SERVER&domain=$NEXUS_FQDN&token=$PIHOLE_TOKEN" -X POST $PIHOLE_CUSTOM_DNS_URL
+curl -d "action=add&ip=$DNS_SERVER&domain=$PIHOLE_FQDN&token=$PIHOLE_TOKEN" -b cookies.txt -X POST $PIHOLE_CUSTOM_DNS_URL
+curl -d "action=add&ip=$DNS_SERVER&domain=$NEXUS_FQDN&token=$PIHOLE_TOKEN" -b cookies.txt -X POST $PIHOLE_CUSTOM_DNS_URL
 
 # create proxy network for traefik
 docker network create proxy
 
-TRAEFIK_CREDS=$(htpasswd -nb "admin" "$TRAEFIK_PASSWORD") | sed -e s/\\$/\\$\\$/g
+TRAEFIK_CREDS=$(htpasswd -nb "admin" "$TRAEFIK_PASSWORD" | sed -e s/\\$/\\$\\$/g)
 sed -i "s/traefik.http.middlewares.traefik-auth.basicauth.users=USER:BASIC_AUTH_PASSWORD/traefik.http.middlewares.traefik-auth.basicauth.users=$TRAEFIK_CREDS/g" traefik/docker-compose.yml
-docker-compose -f traefik/docker-compose.yml -p traefik up -d
-docker-compose -f traefik/docker-compose.yml -p traefik up -d --force-recreate
 
 # download and install docker-compose
 curl -L "https://github.com/docker/compose/releases/download/v2.24.2/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 chmod 755 /usr/local/bin/docker-compose
+
+docker-compose -f traefik/docker-compose.yml -p traefik up -d
+docker-compose -f traefik/docker-compose.yml -p traefik up -d --force-recreate
+
 
 
 # install traefik into docker container and use letsencrypt for ssl certificates via cloudflare
