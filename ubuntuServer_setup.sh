821db7c8863f8a076e6bf3c9416e6453bb7a5037
Darien Fodor <dafodor42@gmail.com>
2024-02-01 10:09:51 -0600

    clearing up the script and commeting out some unneeded code

--- ubuntuServer_setup.sh
+++ ubuntuServer_setup.sh
@@ -6,10 +6,10 @@
 
 #this scripts is to be run on a ubuntu server before anything else has been start.sh
 
-if [ "$(id -u)" != "0" ]; then
-    echo "This script must be run as root."
-    exit 1
-fi
+#if [ "$(id -u)" != "0" ]; then
+#    echo "This script must be run as root."
+#    exit 1
+#fi
 
 sudo apt-get update && sudo apt-get upgrade -y
 
@@ -17,7 +17,7 @@ if dpkg -l | grep -q "openssh-server"; then
     echo "OpenSSH Server is installed."
 else
     echo "OpenSSH Server is not installed."
-    apt install openssh-server
+    sudo apt install openssh-server
 fi
 
 #These are some extra stuff to disable resolved, they shouldn't be needed but are still here just in case.
@@ -31,13 +31,6 @@ sudo curl -L https://github.com/coreos/butane/releases/download/v0.19.0/butane-x
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.2/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose && \
 sudo chmod 755 /usr/local/bin/docker-compose
 git clone https://github.com/cseelhoff/coreos && cd coreos
-#organizing the files
-if ls -al | grep -q ".env"; then
-    echo ".env, the serects directory hasn't been made yet. The start.sh script will promopt that at the start."
-else
-    echo "Net-tools is not installed. QoL feature for you"
-    apt install net-tools
-fi
 
 
 #stops the default the DNS service so that the PIhole doesn't run into issues later on
